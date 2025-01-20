931f0706966f259af19ba8b3bced0c196f766bfb
Nickii <51742504+Nickiita@users.noreply.github.com>
2025-01-20 18:33:28 +1000

    lab4

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", views.test),
     path("review/", views.handle_review, name="handle_review"),
     path("review_success/", views.review_success, name="review_success"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login_user"),
+    path("logout/", views.logout_user, name="logout_user"),   
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Person, Review
 from django.contrib.auth.models import User
-
-#from django.contrib.auth import User
+from django.contrib.auth import login, logout, authenticate
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 def test(request):
@@ -42,3 +43,43 @@ def handle_review(request):
 
 def review_success(request):
     return render(request, "review_success.html")
+
+
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("username")
+        password = data.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"error": "Username already exists"}, status=400)
+        
+        User.objects.create_user(username=username, password=password)
+        return JsonResponse({"message": "User registrated successfully"}, status=201)
+
+    return JsonResponse({'error': 'Invalid request.'}, status=405)
+
+
+@csrf_exempt
+def login_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("username")
+        password = data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return JsonResponse({"message": "Login successful"}, status=200)
+        else:
+            return JsonResponse({"error": "Invalid credentials"}, status=401)
+        
+    return JsonResponse({'error': 'Invalid request.'}, status=405)
+        
+
+@csrf_exempt
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return JsonResponse({"message": "Logout successful"}, status=200)
