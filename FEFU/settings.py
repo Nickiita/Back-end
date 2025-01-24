b4ae8c6908f0730cfe3f0589acbdd1748852fda7
Nickii <51742504+Nickiita@users.noreply.github.com>
2025-01-24 15:16:29 +1000

    fix BASE_DIR

--- FEFU/settings.py
+++ FEFU/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,12 +53,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'FEFU.urls'
 
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["os.path.join(BASE_DIR, 'FEFU', 'Templates')"],
+        'DIRS': [BASE_DIR/ 'FEFU' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
